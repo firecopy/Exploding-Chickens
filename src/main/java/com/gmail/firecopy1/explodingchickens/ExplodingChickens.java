39c7e9dd352838bdfcdd5294eaf8b3c7abb43fc1
ryanclancy000 <ryanclancy000@gmail.com>
2012-08-18 20:10:42 -0300

    Cleanup

--- src/main/java/com/gmail/firecopy1/explodingchickens/ExplodingChickens.java
+++ src/main/java/com/gmail/firecopy1/explodingchickens/ExplodingChickens.java
@@ -7,25 +7,28 @@ package com.gmail.firecopy1.explodingchickens;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ExplodingChickens extends JavaPlugin {
-    public int ExplosionDamage;
-    boolean FireToggle;
-    private final DamageListener dListener = new DamageListener(this);
+    
+    private int explosionDamage;
+    private boolean fireToggle;
 
     @Override
     public void onEnable() {
         loadConfig();
-        registerEvents();
-    }
-
-    private void registerEvents() {
-        this.getServer().getPluginManager().registerEvents(dListener, this);
+        getServer().getPluginManager().registerEvents(new DamageListener(this), this);
     }
 
     private void loadConfig() {
-        this.getConfig().options().copyDefaults(true);
-        ExplosionDamage = this.getConfig().getInt("ExplosionDamage", 8);
-        FireToggle = this.getConfig().getBoolean("FireToggle", false);
-        
+        getConfig().options().copyDefaults(true);
+        explosionDamage = this.getConfig().getInt("ExplosionDamage", 8);
+        fireToggle = this.getConfig().getBoolean("FireToggle", false);
         saveConfig();
+    }
+    
+    public int getDamage() {
+        return explosionDamage;
+    }
+    
+    public boolean isFireToggle() {
+        return fireToggle;
     }
 }
